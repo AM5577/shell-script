458bdd753e64313a80aa2d22c63cb71dcd58140c
Arun M <arun.maddukuri07@gmail.com>
2025-10-16 13:34:56 +0530

    added special variables script

--- 4.special-variables.sh
+++ 4.special-variables.sh
@@ -5,4 +5,5 @@ echo "script name: $0"
 echo "present working directory: $PWD"
 echo "directory of current user $USER"
 echo "process id of curent script $$"
+sleep 60 &
 echo "process id of background script $!"
