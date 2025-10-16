5071ab82ea58d5d0efa3a4e19edf7eb6553af7d0
Arun M <arun.maddukuri07@gmail.com>
2025-10-16 13:26:28 +0530

    added special variables script

---
+++ 4.special-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "All variables passed: $@"
+echo "number of variables: $#"
+echo "script name: $0"
+echo "present working directory: $PWD"
+echo "directory of current user $USER"
+echo "process id of curent script $$"
+echo "process id of background script $!"
