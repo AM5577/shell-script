4c0ea72d61c98ab87beafa59f84e07dab12ec097
Arun M <arun.maddukuri07@gmail.com>
2025-10-20 19:42:32 +0530

    updated var.sh

--- 11-install-script.sh
+++ 11-install-script.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-USERID=(id -u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
     echo "Please run the script with sudo user"
+    exit 1
 fi
 
 dnf list installed wget
+
 if [ $? -ne 0 ]
 then
     dnf install wget -y
     if [ $? -ne 0 ]
     then
         echo "Installtion ... Failure"
+        exit 1
     else
         echo "Installation ...Sucess"
     fi
