7daf13520a2c436738a94c63d7dd917e8c33be63
Arun M <arun.maddukuri07@gmail.com>
2025-10-20 19:23:07 +0530

    updated var.sh

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -10,31 +10,31 @@ fi
 
 dnf list installed mysql
 
-if [$? -ne 0 ]
-    dnf install mysql -y
+if [ $? -ne 0 ]
 then
-    echo "Installing mysql is not successful .. FAILURE"
-    exit 1
-else
-    echo "Installing mysql is succesfful ... SUCCESS"
-fi
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+         echo "Installing mysql is not successful .. FAILURE"
+        exit 1
+    else
+         echo "Installing mysql is succesfful ... SUCCESS"
+    fi
 else
     echo "Mysql is already installed"
 fi
 
-
-# if [ $? -ne 0 ]
-# then
-#     echo "Installing mysql is not successful .. FAILURE"
-#     exit 1
-# else
-#     echo "Installing mysql is succesfful ... SUCCESS"
-# fi
-
+dnf list installed git
 if [ $? -ne 0 ]
 then
-    echo "Installing git is not succesfull ...FAILURE"
-    exit 1
-    echo "Installing git successfull...SUCCESS"
+     dnf install git -y
+     if [ $? -ne 0 ]
+     then
+          echo "Installation git .. FAILURE"
+          exit 1
+      else
+          echo "Installation git .. SUCCESS"
+     fi
+else
+     echo "GIT is already installed"
 fi
-dnf install git -y
