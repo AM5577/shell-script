9e5f55cce3a99871631c511fdaa8722708e87e75
Arun M <arun.maddukuri07@gmail.com>
2025-10-20 18:27:44 +0530

    updated var.sh

--- 5-conditions.sh
+++ 5-conditions.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+
 NUMBER=$1
- #-gt -lt -eq -ge -le 
+
 if [ $NUMBER -gt 100 ]
 then
-    echo "Given number is greater than 100"
+    echo "given number is greater than 100"
 else
-    echo "Given number is less than or equal to 100"
-fi 
+    echo "given number is less than or equal to 100"
+fi
